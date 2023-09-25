4983eca9030e03bd40bb57ecee1e707411b95a5d
Sajal Gupta <sajal07091999@gmail.com>
2023-09-26 00:29:40 +0530

    Ep03 - Laying the Foundation

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"},[
-    React.createElement("div",{id: "child1"},
-    [React.createElement("h1",{},"h1"), React.createElement("h2",{},"h2")]),
-    React.createElement("div",
-    {id: "child2"},
-    [React.createElement("h1",{},"h1"), React.createElement("h2",{},"h2")])]    
+//React.createElement => Object => HTMLElement(render)
+
+//React Functional Component
+const Title = () => (
+    <h1 className = "head" tabIndex="5">
+        HI 
+    </h1>
+    
 );
+
+
+//Component Composition
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <Title></Title>
+        {Title()}
+        <h1>BYE ...</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+//root.render(jsxHeading);
+root.render(<HeadingComponent />);
